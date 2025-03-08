ef022ea01c5338ade30e256bbc9939c197a1590d
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-03-08 12:50:02 +0200

    fixed a letter in the query for creating table

--- Server/migrations/m002_faqs.php
+++ Server/migrations/m002_faqs.php
@@ -4,7 +4,7 @@ require __DIR__."../connection/connection.php";
 
 $query = ("CREATE TABLE faqs(
             id INT(11) AUTO_INCREMENT PRIMARY KEY,
-            question VARCHAR(255) NO NULL,
+            question VARCHAR(255) NOT NULL,
             answer VARCHAR(255) NOT NULL)");
 
 $start = $conn->prepare($query);
