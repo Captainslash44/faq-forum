c9147e37bed0a21c839dd8e008d6b1bd13678c95
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-03-09 16:39:00 +0200

    completed the seeds file

---
+++ Server/models/Faq.php
@@ -0,0 +1,38 @@
+<?php
+
+require("FaqSkeleton.php");
+require("../connection/connection.php");
+
+
+class Faq extends FaqSkeleton{
+
+    public static function saveFaq(){
+        global $conn;
+
+        $query = $conn->prepare("INSERT INTO faqs (question, answer) VALUES (?,?)");
+        $query->bind_param("ss", self::$question, self::$answer);
+        $query->execute();
+
+        return "faq added";
+    }
+
+
+    public static function getAllFaqs(){
+        global $conn;
+
+        $query = $conn->prepare("SELECT * FROM faqs");
+        $query->execute();
+
+        $response = $query->get_result();
+        $answer = [];
+        while($i = $response->fetch_assoc()){
+            $answer[] = $i;
+        }
+
+        return $answer;
+    }
+
+
+};
+
+?>
